a9b2ee92827ee9ae86f54f15026a75c70f96b9d8
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-04 17:04:01 -0300

    feat: adds bodyParser to handle JSON data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.ywtr0jv.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoUri);
+
 mongoose.connection.on('connected', () => {
     console.log('Connected to mongo instance');
 });
+
 mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err);
 });
