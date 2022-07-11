c7698bb103a814138b8ead3a6b2541b126a54f6b
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-11 10:28:04 -0300

    feat: lists tracks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.ywtr0jv.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoUri);
@@ -26,5 +29,5 @@ app.get('/', requireAuth, (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log ('Listening on port 3000');
+    console.log('Listening on port 3000');
 });
